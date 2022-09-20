071d8f10da9338534271d901dfe894bf5d0b029a
Matt C <matthew.compton@mymail.champlain.edu>
2022-09-20 18:28:37 +0000

    basic skeleton

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ytm-dump
 Dump all songs from all youtube music playlists, and download them all
+
+## Setup
+https://ytmusicapi.readthedocs.io/en/latest/setup.html#authenticated-requests
---
+++ rip.py
@@ -0,0 +1,44 @@
+import os,sys
+
+from ytmusicapi import YTMusic
+
+def check_song(title, id):
+    files = os.listdir()
+    found = False
+    for file in files:
+        if title in file and id in file:
+            found = True
+    return found
+
+if not os.path.exists('headers_auth.json'):
+    print("please run the setup")
+    sys.exit(1)
+
+ytmusic = YTMusic('headers_auth.json')
+
+playlists = ytmusic.get_library_playlists()
+
+pl_ids = []
+
+for playlist in playlists:
+    pl_ids.append(playlist['playlistId'])
+
+for pl_id in pl_ids:
+    data = ytmusic.get_playlist(pl_id)
+    title = data['title']
+    print(f"Starting {title}")
+    if not os.path.exists(title):
+        os.makedirs(title)
+    os.chdir(title)
+    tracks = data['tracks']
+    for track in tracks:
+        t_title = track['title']
+        vid = track['videoId']
+        print(f"Downloading {vid}")
+        url = f"https://www.youtube.com/watch?v={vid}"
+        if not check_song(t_title, vid):
+            print("Didn't find file, downloading.")
+            os.system(f"youtube-dl {url}")
+        else:
+            print("Not downloading.")
+    os.chdir("../")
