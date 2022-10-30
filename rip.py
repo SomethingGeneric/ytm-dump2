5eb863b7c3cd5377b8d794bc386c821c668fa3c5
Matt C <matthew.compton@mymail.champlain.edu>
2022-10-29 22:49:26 -0400

    I'm big dumb

--- rip.py
+++ rip.py
@@ -52,16 +52,17 @@ for pl_id in pl_ids:
         t_title = track['title']
         vid = track['videoId']
         if t_title is None or vid is None:
-            pass
-        print(f"Downloading {vid}")
-        url = f"https://music.youtube.com/watch?v={vid}"
-        if not check_song(t_title, vid):
-            try:
-                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                    ydl.download([url])
-            except Exception as e:
-                print(f"Failed to download {url}: {str(e)}")
-                failed.append(url)
+            print(f"Skipping {t_title} or {vid}")
+        else:
+            print(f"Downloading {vid}")
+            url = f"https://music.youtube.com/watch?v={vid}"
+            if not check_song(t_title, vid):
+                try:
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        ydl.download([url])
+                except Exception as e:
+                    print(f"Failed to download {url}: {str(e)}")
+                    failed.append(url)
     os.chdir("../")
 
 with open("failed.txt", "w") as f:
